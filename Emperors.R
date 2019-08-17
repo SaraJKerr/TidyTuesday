987b985c32693b9cd36bc0d0bed46fa9112ceef9
Sara J Kerr <SaraJKerr@users.noreply.github.com>
2019-08-17 14:53:57 +0100

    Made Treemap interactive

--- Emperors.R
+++ Emperors.R
@@ -5,28 +5,31 @@ library(readr)
 library(lubridate)
 library(ggthemes)
 library(treemap)
-library(plotly)
+library(d3treeR)
 
 # Load data
 emperors <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-13/emperors.csv")
 
 # Make summary dataframe
 emp_death <- emperors %>% 
-        group_by(dynasty, cause) %>% 
+        group_by(dynasty, cause, name) %>% 
         summarise(Count = n())
 
 # Create treemap
-treemap(emp_death,
-        index=c("dynasty","cause"),
+emp_tree <-  treemap(emp_death,
+        index=c("dynasty","cause", "name"),
         vSize="Count",
         type="index",
         border.col=c("black","white"),
         palette = "Set1",
         title="Roman Emperors: Cause of death by dynasty",                      
-        fontsize.title=14
-) 
+        fontsize.title=16
+)
 
+emp_int <- d3tree(emp_tree,  rootname = "Emperors")
 
+library(htmlwidgets)
+saveWidget(emp_int, file="interactiveTreemap.html", selfcontained = T)
 
 
 
